48ee7d2d104cdcc3d3a9f1df052d0b030bb35c13
Angel Valladares <arivalladares2.0@gmail.com>
2024-04-11 11:21:52 -0600

    Refactor routing in App.jsx to use react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,30 @@
-
-import Hero from './components/Hero'
-import NewestDeals from './components/NewestDeals'
-import AboutUs from './components/AboutUs'
-import BestDeals from './components/BestDeals'
-import Footer from './components/Footer'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Hero from './components/Hero';
+import NewestDeals from './components/NewestDeals';
+import AboutUs from './components/AboutUs';
+import BestDeals from './components/BestDeals';
+import Footer from './components/Footer';
+import Login from './components/Login';
 
 export default function App() {
-
   return (
-    <>
-    <div className="App  w-11/12 md:w-4/5 m-auto">
-      <Hero/>
-      <NewestDeals/>
-      <AboutUs/>
-      <BestDeals/>
-    </div>
-      <Footer/>
-    </>
-  )
+    <Router>
+      <div className="App w-11/12 md:w-4/5 m-auto">
+        <Routes>
+          <Route path="/" element={
+            <>
+              <Hero />
+              <NewestDeals />
+              <AboutUs />
+              <BestDeals />
+            </>
+          } />
+          <Route path="/login" element={<Login/>} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </div>
+      <Footer />
+    </Router>
+  );
 }
-
