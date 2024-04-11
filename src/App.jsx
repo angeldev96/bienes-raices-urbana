7e58b4b48122ed70dc299dfe9dd7a3d3af6497a3
Angel Valladares <arivalladares2.0@gmail.com>
2024-04-11 10:39:48 -0600

    Add firebase dependency

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+
 import Hero from './components/Hero'
 import NewestDeals from './components/NewestDeals'
 import AboutUs from './components/AboutUs'
