be226d2070f09cdb2d4555662f24a4329c64ff00
Angel Valladares <arivalladares2.0@gmail.com>
2024-04-11 13:33:02 -0600

    Update navigation and button styles in Hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,15 +2,17 @@ import image from "../assets/image6.jpg";
 const Hero = () => {
   return (
     <div className="">
-      <nav className="flex justify-between items-center py-5">
-        <h1 className="text-xl md:text-2xl font-bold">Bienes Raíces Urbana</h1>
-        <ul className="flex items-center space-x-5 text-xs md:text-base">
-          <li>Características</li>
-          <li>Tendencias</li>
-          <li>Acerca de</li>
-        </ul>
-        <button className="bg-cyan-900 px-5 py-2 rounded-xl text-xs md:text-base text-gray-300">Contacto</button>
-      </nav>
+   <nav className="flex justify-between items-center py-5">
+  <h1 className="text-xl md:text-2xl font-bold">Bienes Raíces Urbana</h1>
+  <ul className="flex items-center space-x-5 text-xs md:text-base">
+  <li><a href="/contacto" className="hover:underline">Caracteristicas</a></li>
+  <li><a href="/contacto" className="hover:underline">Tendencias</a></li>
+    <li><a href="/contacto" className="hover:underline">Contacto</a></li>
+  </ul>
+  <div className="flex space-x-4">
+  <button className="bg-white border border-cyan-900 px-5 py-2 rounded-xl text-xs md:text-base text-cyan-900 hover:bg-gray-200">Iniciar sesión</button>  <button className="bg-cyan-900 px-5 py-2 rounded-xl text-xs md:text-base text-gray-300 hover:bg-cyan-700">Registrarse</button>
+  </div>
+</nav>
       <div className="h-[500px] relative">
         <img
           src={image}
