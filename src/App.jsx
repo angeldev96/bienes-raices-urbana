af9836353ab2f31e885f887a7b964e04a84463eb
Angel Valladares <arivalladares2.0@gmail.com>
2024-04-11 12:27:13 -0600

    Refactor routing in App.jsx to remove unused import

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Hero from './components/Hero';
 import NewestDeals from './components/NewestDeals';
