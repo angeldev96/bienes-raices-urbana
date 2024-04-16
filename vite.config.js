9aaa76d904ef5a507cfa9043f1ba3e116746742d
Angel Valladares <arivalladares2.0@gmail.com>
2024-04-16 01:40:12 -0600

    migration from react to vue

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,16 @@
+import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  esbuild: {
-    jsxInject: `import React from 'react'`,
-  },
+  plugins: [
+    vue(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
 })
