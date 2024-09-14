71d2b1bf39200676b6176106ba07272719727c87
Angel Valladares <arivalladares2.0@gmail.com>
2024-09-14 12:23:03 -0600

    Refactor import statements in main.jsx and remove unused code

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { initializeApp } from "firebase/app";
-import { getAuth, signInWithEmailAndPassword, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+// import { initializeApp } from "firebase/app";
+// import { getAuth, signInWithEmailAndPassword, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY,
@@ -15,163 +15,163 @@ const firebaseConfig = {
 
 
 
-const app = initializeApp(firebaseConfig);
+// const app = initializeApp(firebaseConfig);
 
-const auth = getAuth(app);
+// const auth = getAuth(app);
 
 
 export default function Login() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const navigate = useNavigate();
+  // const [email, setEmail] = useState('');
+  // const [password, setPassword] = useState('');
+  // const navigate = useNavigate();
 
 
-  const handleEmailLogin = async (e) => {
-    e.preventDefault();
-    try {
-      await signInWithEmailAndPassword(auth, email, password);
-      navigate('/'); // Redirige al usuario a la página de inicio
+  // const handleEmailLogin = async (e) => {
+  //   e.preventDefault();
+  //   try {
+  //     await signInWithEmailAndPassword(auth, email, password);
+  //     navigate('/'); // Redirige al usuario a la página de inicio
 
-    } catch (error) {
-      console.error("Error al iniciar sesión con email y contraseña:", error);
-      // Maneja el error de inicio de sesión
-    }
-  };
+  //   } catch (error) {
+  //     console.error("Error al iniciar sesión con email y contraseña:", error);
+  //     // Maneja el error de inicio de sesión
+  //   }
+  // };
 
-  const handleGoogleLogin = async () => {
-    try {
-      const provider = new GoogleAuthProvider();
-      await signInWithPopup(auth, provider);
-      navigate('/'); // Redirige al usuario a la página de inicio
-    } catch (error) {
-      console.error("Error al iniciar sesión con Google:", error);
-      // Maneja el error de inicio de sesión
-    }
-  };
+  // const handleGoogleLogin = async () => {
+  //   try {
+  //     const provider = new GoogleAuthProvider();
+  //     await signInWithPopup(auth, provider);
+  //     navigate('/'); // Redirige al usuario a la página de inicio
+  //   } catch (error) {
+  //     console.error("Error al iniciar sesión con Google:", error);
+  //     // Maneja el error de inicio de sesión
+  //   }
+  // };
 
-  return (
-    <>
-      <div className="h-full bg-white">
-        <div className="h-full">
-          <div className="flex min-h-full flex-1">
-            <div className="flex flex-1 flex-col justify-center px-4 py-12 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
-              <div className="mx-auto w-full max-w-sm lg:w-96">
-                <div>
-                  <img
-                    className="h-10 w-auto"
-                    src="public/urbana.jpg"
-                    alt="Bienes Raices Urbana"
-                  />
-                  <h2 className="mt-8 text-2xl font-bold leading-9 tracking-tight text-gray-900">
-                    Inicia sesión en tu cuenta
-                  </h2>
+  // return (
+  //   <>
+  //     <div className="h-full bg-white">
+  //       <div className="h-full">
+  //         <div className="flex min-h-full flex-1">
+  //           <div className="flex flex-1 flex-col justify-center px-4 py-12 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
+  //             <div className="mx-auto w-full max-w-sm lg:w-96">
+  //               <div>
+  //                 <img
+  //                   className="h-10 w-auto"
+  //                   src="public/urbana.jpg"
+  //                   alt="Bienes Raices Urbana"
+  //                 />
+  //                 <h2 className="mt-8 text-2xl font-bold leading-9 tracking-tight text-gray-900">
+  //                   Inicia sesión en tu cuenta
+  //                 </h2>
                 
-                </div>
+  //               </div>
 
-                <div className="mt-10">
-                  <div>
-                    <form onSubmit={handleEmailLogin} className="space-y-6">
-                      <div>
-                        <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
-                          Correo electrónico
-                        </label>
-                        <div className="mt-2">
-                          <input
-                            id="email"
-                            name="email"
-                            type="email"
-                            autoComplete="email"
-                            required
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                            className="block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                          />
-                        </div>
-                      </div>
+  //               <div className="mt-10">
+  //                 <div>
+  //                   <form onSubmit={handleEmailLogin} className="space-y-6">
+  //                     <div>
+  //                       <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+  //                         Correo electrónico
+  //                       </label>
+  //                       <div className="mt-2">
+  //                         <input
+  //                           id="email"
+  //                           name="email"
+  //                           type="email"
+  //                           autoComplete="email"
+  //                           required
+  //                           value={email}
+  //                           onChange={(e) => setEmail(e.target.value)}
+  //                           className="block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+  //                         />
+  //                       </div>
+  //                     </div>
 
-                      <div>
-                        <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
-                          Contraseña
-                        </label>
-                        <div className="mt-2">
-                          <input
-                            id="password"
-                            name="password"
-                            type="password"
-                            autoComplete="current-password"
-                            required
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                            className="block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                          />
-                        </div>
-                      </div>
+  //                     <div>
+  //                       <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+  //                         Contraseña
+  //                       </label>
+  //                       <div className="mt-2">
+  //                         <input
+  //                           id="password"
+  //                           name="password"
+  //                           type="password"
+  //                           autoComplete="current-password"
+  //                           required
+  //                           value={password}
+  //                           onChange={(e) => setPassword(e.target.value)}
+  //                           className="block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+  //                         />
+  //                       </div>
+  //                     </div>
 
-                      <div className="flex items-center justify-between">
+  //                     <div className="flex items-center justify-between">
                      
 
-                        <div className="text-sm leading-6">
-                          <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
-                            ¿Olvidaste tu contraseña?
-                          </a>
-                        </div>
-                      </div>
+  //                       <div className="text-sm leading-6">
+  //                         <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
+  //                           ¿Olvidaste tu contraseña?
+  //                         </a>
+  //                       </div>
+  //                     </div>
 
-                      <div>
-                        <button
-                          type="submit"
-                          className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                        >
-                          Iniciar sesión
-                        </button>
-                      </div>
-                    </form>
-                  </div>
+  //                     <div>
+  //                       <button
+  //                         type="submit"
+  //                         className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+  //                       >
+  //                         Iniciar sesión
+  //                       </button>
+  //                     </div>
+  //                   </form>
+  //                 </div>
 
-                  <div className="mt-10">
-                    <div className="relative">
-                      <div className="absolute inset-0 flex items-center" aria-hidden="true">
-                        <div className="w-full border-t border-gray-200" />
-                      </div>
-                      <div className="relative flex justify-center text-sm font-medium leading-6">
-                        <span className="bg-white px-6 text-gray-900">O inicia sesion con</span>
-                      </div>
-                    </div>
+  //                 <div className="mt-10">
+  //                   <div className="relative">
+  //                     <div className="absolute inset-0 flex items-center" aria-hidden="true">
+  //                       <div className="w-full border-t border-gray-200" />
+  //                     </div>
+  //                     <div className="relative flex justify-center text-sm font-medium leading-6">
+  //                       <span className="bg-white px-6 text-gray-900">O inicia sesion con</span>
+  //                     </div>
+  //                   </div>
 
-                    <div className="mt-6 grid grid-cols-2 gap-4">
-                      <a
-                        href="#"
-                        onClick={handleGoogleLogin}
-                        className="flex w-full items-center justify-center gap-3 border border-gray-300 rounded-md bg-white px-3 py-1.5 text-black focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[#1D9BF0]"
-                      >
-                        <svg className="h-5 w-5" aria-hidden="true" viewBox="0 0 24 24"><path d="M12.0003 4.75C13.7703 4.75 15.3553 5.36002 16.6053 6.54998L20.0303 3.125C17.9502 1.19 15.2353 0 12.0003 0C7.31028 0 3.25527 2.69 1.28027 6.60998L5.27028 9.70498C6.21525 6.86002 8.87028 4.75 12.0003 4.75Z" fill="#EA4335"></path><path d="M23.49 12.275C23.49 11.49 23.415 10.73 23.3 10H12V14.51H18.47C18.18 15.99 17.34 17.25 16.08 18.1L19.945 21.1C22.2 19.01 23.49 15.92 23.49 12.275Z" fill="#4285F4"></path><path d="M5.26498 14.2949C5.02498 13.5699 4.88501 12.7999 4.88501 11.9999C4.88501 11.1999 5.01998 10.4299 5.26498 9.7049L1.275 6.60986C0.46 8.22986 0 10.0599 0 11.9999C0 13.9399 0.46 15.7699 1.28 17.3899L5.26498 14.2949Z" fill="#FBBC05"></path><path d="M12.0004 24.0001C15.2404 24.0001 17.9654 22.935 19.9454 21.095L16.0804 18.095C15.0054 18.82 13.6204 19.245 12.0004 19.245C8.8704 19.245 6.21537 17.135 5.2654 14.29L1.27539 17.385C3.25539 21.31 7.3104 24.0001 12.0004 24.0001Z" fill="#34A853"></path></svg>
-                        <span className="text-sm font-semibold leading-6">Google</span>
-                      </a>
+  //                   <div className="mt-6 grid grid-cols-2 gap-4">
+  //                     <a
+  //                       href="#"
+  //                       onClick={handleGoogleLogin}
+  //                       className="flex w-full items-center justify-center gap-3 border border-gray-300 rounded-md bg-white px-3 py-1.5 text-black focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[#1D9BF0]"
+  //                     >
+  //                       <svg className="h-5 w-5" aria-hidden="true" viewBox="0 0 24 24"><path d="M12.0003 4.75C13.7703 4.75 15.3553 5.36002 16.6053 6.54998L20.0303 3.125C17.9502 1.19 15.2353 0 12.0003 0C7.31028 0 3.25527 2.69 1.28027 6.60998L5.27028 9.70498C6.21525 6.86002 8.87028 4.75 12.0003 4.75Z" fill="#EA4335"></path><path d="M23.49 12.275C23.49 11.49 23.415 10.73 23.3 10H12V14.51H18.47C18.18 15.99 17.34 17.25 16.08 18.1L19.945 21.1C22.2 19.01 23.49 15.92 23.49 12.275Z" fill="#4285F4"></path><path d="M5.26498 14.2949C5.02498 13.5699 4.88501 12.7999 4.88501 11.9999C4.88501 11.1999 5.01998 10.4299 5.26498 9.7049L1.275 6.60986C0.46 8.22986 0 10.0599 0 11.9999C0 13.9399 0.46 15.7699 1.28 17.3899L5.26498 14.2949Z" fill="#FBBC05"></path><path d="M12.0004 24.0001C15.2404 24.0001 17.9654 22.935 19.9454 21.095L16.0804 18.095C15.0054 18.82 13.6204 19.245 12.0004 19.245C8.8704 19.245 6.21537 17.135 5.2654 14.29L1.27539 17.385C3.25539 21.31 7.3104 24.0001 12.0004 24.0001Z" fill="#34A853"></path></svg>
+  //                       <span className="text-sm font-semibold leading-6">Google</span>
+  //                     </a>
 
-                      <a
-                        href="#"
-                        className="flex w-full items-center justify-center gap-3 rounded-md bg-blue-500 px-3 py-1.5 text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[#24292F]"
-                      >
-                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-facebook" viewBox="0 0 16 16">
-                          <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z" />
-                        </svg>
-                        <span className="text-sm font-semibold leading-6">Facebook</span>
-                      </a>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="relative hidden w-0 flex-1 lg:block">
-              <img
-                className="absolute inset-0 h-15 w-15 object-cover"
-                src="public/urbana.jpg"
-                alt=""
-              />
-            </div>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+  //                     <a
+  //                       href="#"
+  //                       className="flex w-full items-center justify-center gap-3 rounded-md bg-blue-500 px-3 py-1.5 text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[#24292F]"
+  //                     >
+  //                       <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-facebook" viewBox="0 0 16 16">
+  //                         <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z" />
+  //                       </svg>
+  //                       <span className="text-sm font-semibold leading-6">Facebook</span>
+  //                     </a>
+  //                   </div>
+  //                 </div>
+  //               </div>
+  //             </div>
+  //           </div>
+  //           <div className="relative hidden w-0 flex-1 lg:block">
+  //             <img
+  //               className="absolute inset-0 h-15 w-15 object-cover"
+  //               src="public/urbana.jpg"
+  //               alt=""
+  //             />
+  //           </div>
+  //         </div>
+  //       </div>
+  //     </div>
+  //   </>
+  // );
 }
--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,21 +1,21 @@
 import { useEffect, useState } from 'react';
 import { Navigate } from 'react-router-dom';
-import { getAuth, onAuthStateChanged } from "firebase/auth";
+// import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 const ProtectedRoute = ({ children }) => {
     const [usuario, setUsuario] = useState(null);
     const [cargando, setCargando] = useState(true);
 
     useEffect(() => {
-        const auth = getAuth();
-        onAuthStateChanged(auth, (user) => {
-            if (user) {
-                setUsuario(user);
-            } else {
-                setUsuario(null);
-            }
-            setCargando(false);
-        });
+        // const auth = getAuth();
+        // onAuthStateChanged(auth, (user) => {
+        //     if (user) {
+        //         setUsuario(user);
+        //     } else {
+        //         setUsuario(null);
+        //     }
+        //     setCargando(false);
+        // });
     }, []);
 
     if (cargando) {
